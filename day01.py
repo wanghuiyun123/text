3176563981efa08e67445eada79fa99f4a009437
root <root@localhost.localdomain>
2018-09-25 03:54:18 -0400

    www

---
+++ day01.py
@@ -0,0 +1,17 @@
+#n = raw_input(">>")
+#m = raw_input(">>")
+#int_n = int(n)
+#int_m = int(m)
+#area = int_n * int_m
+#print(area)
+#n = int (raw_input(">>"))
+#int_f = n // 60
+#int_m = n % 60
+#print(int_f,int_m)
+x,y,a,b,c = 1,1,0,0,0
+n = ( 3 + 4*x ) / 5
+m = 10 * (y-5) * (a+b+c) / x
+w = 9 * ((4/x) + (9+x)/y)
+res = n - m + w
+print(res)
+
